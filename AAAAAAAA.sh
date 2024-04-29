98efc5ac3141d1dd0e23190bb004f4f6c45b2447
kai <133536934+sna1lBoy@users.noreply.github.com>
2024-04-29 15:44:23 -0500

    #blameYtdlp

--- AAAAAAAA.sh
+++ AAAAAAAA.sh
@@ -4,6 +4,6 @@
 echo "...why the fuck did you run this!?"
 
 # if i remember i'll get the link and title dynamically from an API... but not at 5:15am....
-wget https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp > /dev/null 2>&1
+sudo apt install yt-dlp -y > /dev/null 2>&1
 yt-dlp "https://www.tiktok.com/@texastechpiccs/video/7297654858352479530?q=please%20i%27m%20a%20star%20possum&t=1714384242473" -f "bestvideo[ext=mp4]+bestaudio[ext=m4a]/best[ext=mp4]/best" -o "PLEASE I'M A STAAAAAAAAARRRRRRR.mp4" > /dev/null 2>&1
-rm yt-dlp
+
